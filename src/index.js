d78c2dd70f635b2905c406becb71fbd487999fb4
LouBell14 <87602844+LouBell14@users.noreply.github.com>
2023-11-25 06:37:59 +0000

    Updated JS to call live day in forecast

--- src/index.js
+++ src/index.js
@@ -54,6 +54,12 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function formatDay(timestamp) {
+  let date = new Date (timestamp *1000);
+  Let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+return days [date.getDay()];
+}
+
 function getForecast(city) {
   let apiKey = "aft4c703eb4e588f70o3063bb1c00a8b";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
@@ -69,7 +75,7 @@ function displayForecast(response) {
         forecastHtml +
         `
   <div class="weather-forecast-day">
-    <div class="weather-forecast-date">Tues</div>
+    <div class="weather-forecast-date">${formatDay(day.time)}</div>
     <img src="${day.condition.icon_url}" class="weather-forecast-icon"/>
     <div class="weather-forecast-temperatures">
       <div class="weather-forecast-temperature">
