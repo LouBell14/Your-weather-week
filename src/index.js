0c90a4493fe6736cdf848cee539a08925a7daf42
LouBell14 <87602844+LouBell14@users.noreply.github.com>
2023-11-25 06:18:52 +0000

    Updated js with live weather icons

--- src/index.js
+++ src/index.js
@@ -69,7 +69,9 @@ function displayForecast(response) {
       `
   <div class="weather-forecast-day">
     <div class="weather-forecast-date">Tues</div>
-    <div class="weather-forecast-icon">☀️</div>
+    <div class="weather-forecast-icon">
+    <img src="${day.condition.icon_url}"/>
+    </div>
     <div class="weather-forecast-temperatures">
       <div class="weather-forecast-temperature">
         <strong>${Math.round(day.temperature.maximum)}°</strong>
